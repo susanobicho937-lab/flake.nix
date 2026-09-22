b0e7aad44a3a801c71904265f06a2e1e3b0e201c
susanobicho937-lab <susanobicho937@gmail.com>
2026-09-21 22:31:14 -0600

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -2,12 +2,24 @@
   description = "papoi";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    pyroclear.url = "github:shreyanth-sureshkrishnaa/pyroclear";
+    noctalia = {
+      url = "github:noctalia-dev/noctalia";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.salsa-y-picante = nixpkgs.lib.nixosSystem {
+
+  outputs = { self, nixpkgs, pyroclear, ... }@inputs: {
+    nixosConfigurations.salsa-y-picante = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
-      modules = [ ./hardware-configuration.nix ./configuration.nix ];
+      modules = [
+        ./hardware-configuration.nix
+        ./configuration.nix
+        {
+          environment.systemPackages = [ pyroclear.packages.${system}.default ];
+        }
+      ];
     };
   };
 }
